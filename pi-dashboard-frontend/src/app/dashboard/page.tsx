3f79a76b2a95424b8e02c70bae7f31f9843950d9
Javid Ahmed <javidahmed@icloud.com>
2026-01-23 00:16:25 +0000

    Add dashboard page components: SystemInfoWidget and MiniSystemSummary

--- pi-dashboard-frontend/src/app/dashboard/page.tsx
+++ pi-dashboard-frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import MiniSystemSummary from "@/components/dashboard/MiniSystemSummary";
+import SystemInfoWidget from "@/components/dashboard/SystemInfoWidget";
+
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Dashboard page</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      {/* Left Column (2/3 width on large screens) */}
+      <div className="lg:col-span-2 space-y-6">
+        <SystemInfoWidget />
+        <MiniSystemSummary />
+        {/* Service Cards will go here */}
+      </div>
+
+      {/* Right Column (1/3 width on large screens) */}
+      <div className="space-y-6">
+        {/* Weather widget will go here */}
+        {/* Calendar widget will go here */}
+        {/* Notes widget will go here */}
+      </div>
     </div>
   );
 }
